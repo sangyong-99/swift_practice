fcc73903edb5d46796b1ff169e7e47b474069159
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-04-29 18:14:27 +0900

    SplashScreenView 구현

---
+++ main/main/SplashScreenView.swift
@@ -0,0 +1,57 @@
+//
+//  SplashScreenView.swift
+//  Launch_prac1
+//
+//  Created by 신상용 on 2023/04/27.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isActive = false
+    @State private var size = 0.7
+    @State private var opacity = 0.5
+    
+    var body: some View {
+        if isActive {
+            ContentView()
+        } else {
+            VStack{
+                VStack{
+                    Image("familyImage1")
+                        .resizable()
+                        .frame(width: 260, height: 197)
+                        .foregroundColor(.red)
+                    Text("오늘의 은퇴")
+                        .bold()
+                        .font(Font.custom("Baskerbille-Bold", size: 26))
+                        .foregroundColor(.black.opacity(0.80))
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.2)){
+                        self.size = 0.9
+                        self.opacity = 1.0
+                    }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5){
+                    withAnimation {
+                        self.isActive = true
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    
+}
+
+struct SplashScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashScreenView()
+    }
+}
