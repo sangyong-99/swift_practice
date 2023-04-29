5ebb5b882b41520a0efb09e66ec4d2fc2593c808
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-04-30 00:28:09 +0900

    DiaryDetailView2

--- main/main/DiaryDetailView.swift
+++ main/main/DiaryDetailView.swift
@@ -11,46 +11,66 @@ struct DiaryDetailView: View {
     @State var diaryRowData: DiaryListDataStruct
     
     var body: some View {
-        VStack{
-//            ForEach(diaryRowData.image!, id: \.self){image in
-//                image!
-//            }
-//            diaryRowData.image![1] 출력하는 방법
-            
-//            if diaryRowData.image![1] != nil {
-//                Text("yes")
-//            } else {
-//                Text("no")
-//            }
-            
-//            diaryRowData.image![0]
-            
-            let dkdk = diaryRowData.image?.count ?? 0
-            if(diaryRowData.image != nil){
+        ScrollView{
+            VStack{
+                //            ForEach(diaryRowData.image!, id: \.self){image in
+                //                image!
+                //            }
+                //            diaryRowData.image![1] 출력하는 방법
                 
+                //            if diaryRowData.image![1] != nil {
+                //                Text("yes")
+                //            } else {
+                //                Text("no")
+                //            }
+                
+                //            diaryRowData.image![0]
+                
+                let dkdk = diaryRowData.image?.count ?? 0
+                if(diaryRowData.image != nil){
                     
-                
-                
-                ForEach (0..<dkdk){index in
-                    if let imagess = diaryRowData.image![index]{
-                        imagess
-                            .resizable()
-                            .frame(width: 300, height: 300)
-                    }else{
-                        Text("이미지 아직 안올림")
+                    
+                    
+                    
+                    ForEach (0..<dkdk){index in
+                        if index % 2 == 0 {
+                            VStack {
+                                HStack{
+                                    if let imagess = diaryRowData.image![index]{
+                                        imagess
+                                            .resizable()
+                                            .frame(width: 150, height: 150)
+                                    }else{
+                                        Image("noImage")
+                                            .resizable()
+                                            .frame(width: 150, height: 150)
+                                    }
+                                    Spacer()
+                                    if let imagess = diaryRowData.image![index + 1]{
+                                        imagess
+                                            .resizable()
+                                            .frame(width: 150, height: 150)
+                                    }else{
+                                        Image("noImage")
+                                            .resizable()
+                                            .frame(width: 150, height: 150)
+                                    }
+                                }
+                            }
+                            .padding(20)
+                        }
                     }
+                }else {
+                    Text("가족 전체 안함")
                 }
-            }else {
-                Text("\(dkdk)")
-                Text("가족 전체 안함")
             }
+            .padding(.top, 100)
         }
-        
     }
 }
 
 struct DiaryDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DiaryDetailView(diaryRowData: DiaryListDataStruct(page: 1, question: "지금 당장 가고 싶은 곳"))
+        DiaryDetailView(diaryRowData: DiaryListDataStruct(page: 1, question: "지금 당장 가고 싶은 곳", image: [Image("1"), nil, Image("3"), Image("4")]))
     }
 }
