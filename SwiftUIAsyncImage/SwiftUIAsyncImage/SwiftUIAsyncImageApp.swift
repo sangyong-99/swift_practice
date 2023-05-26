a50a50a7e0c2b1c1ccc9dd44e28af29628470e74
‘sangyong-99’ <tkddyd1126@gmail.com>
2023-05-26 17:05:17 +0900

    restart

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by 신상용 on 2023/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
